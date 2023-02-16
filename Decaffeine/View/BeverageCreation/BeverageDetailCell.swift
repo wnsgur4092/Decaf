b0e24a75c2d5d5874552b86c595ee70f4219cdf0
jpazdjara <jpazdjara1@gmail.com>
2023-02-16 12:27:19 +1100

    BeverageDetailView Structure Complete

---
+++ Decaffeine/View/BeverageCreation/BeverageDetailCell.swift
@@ -0,0 +1,36 @@
+//
+//  BeverageDetailCell.swift
+//  Decaffeine
+//
+//  Created by Jordan Pazdjara on 15/2/2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct BeverageDetailCell: View {
+    //MARK: - PROPERTIES
+    var detailImageName : String
+    var detailName : String
+    var detailExtra : String
+    
+    //MARK: - BODY
+    var body: some View {
+        VStack(alignment: .center, spacing: 10){
+            Text(detailName)
+                .font(.system(size: 16))
+                .fontWeight(.bold)
+                .padding(.bottom, 24)
+            Image(detailImageName)
+            
+            Text(detailExtra)
+        }
+        .padding(20)
+    }
+}
+
+struct BeverageDetailCell_Previews: PreviewProvider {
+    static var previews: some View {
+        BeverageDetailCell(detailImageName: "longblack", detailName: "LongBlack", detailExtra: "Large").previewLayout(.sizeThatFits)
+    }
+}
