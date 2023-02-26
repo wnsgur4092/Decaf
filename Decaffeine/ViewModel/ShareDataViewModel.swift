0b3931ba6b40f803940f948df39f07ba3f7751c3
wnsgur4092 <wnsgur4092@gmail.com>
2023-02-26 21:35:14 +1100

    feat. EnvironmentObject

---
+++ Decaffeine/ViewModel/ShareDataViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ShareDataViewModel.swift
+//  Decaffeine
+//
+//  Created by JunHyuk Lim on 26/2/2023.
+//
+
+import Foundation
+import SwiftUI
+
+class shareDataViewModel : ObservableObject {
+    
+    
+}
