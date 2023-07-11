8be2a5a90c2695c44f082014c072b8bb4615bbc7
Jun <wnsgur4092@gmail.com>
2023-07-11 22:43:11 +1000

    feat. NewView

--- Decaffeine/View(Interface)/NewAddView.swift
+++ Decaffeine/View(Interface)/NewAddView.swift
@@ -7,16 +7,35 @@
 
 import SwiftUI
 
+enum Coffee: String, CaseIterable, Identifiable {
+    case Espresso
+    case LongBlack
+    case Macchiato
+    // add more coffee types here as needed
+
+    var id: String { self.rawValue }
+}
+
 struct NewAddView: View {
     //MARK: - PROPERTEIS
     
     //MARK: - BODY
     var body: some View {
         ScrollView{
-            Image("HotCup")
+            Image("Coffee")
                 .resizable()
                 .scaledToFit()
                 .frame(maxWidth: .infinity, maxHeight: 150)
+            
+            VStack{
+                Text("Type")
+                
+                HStack{
+                    ForEach(Coffee.allCases) { coffee in
+                        Text(coffee.rawValue)
+                    }
+                }
+            }
             
         }
         .background(Color("BackgroundColor"))
---
+++ Decaffeine/View(Interface)/TypeSelectView.swift
@@ -0,0 +1,20 @@
+//
+//  TypeSelectView.swift
+//  Decaffeine
+//
+//  Created by JunHyuk Lim on 11/7/2023.
+//
+
+import SwiftUI
+
+struct TypeSelectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TypeSelectView_Previews: PreviewProvider {
+    static var previews: some View {
+        TypeSelectView()
+    }
+}
