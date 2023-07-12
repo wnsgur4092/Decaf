597d2754d7478830b78783b6e8c5648eb5575269
Jun <wnsgur4092@gmail.com>
2023-07-12 22:48:00 +1000

    feat. ListView

---
+++ Decaffeine/View(Interface)/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  Decaffeine
+//
+//  Created by JunHyuk Lim on 12/7/2023.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
