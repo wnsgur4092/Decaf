d0ca42dd3bc618789b6c5452bd47475611ecb788
Jun <wnsgur4092@gmail.com>
2023-07-13 23:04:03 +1000

    update. TypeSelectView

--- Decaffeine/View(Interface)/NewHomeView.swift
+++ Decaffeine/View(Interface)/NewHomeView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct NewHomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack{
+                Text("Click Below")
+                
+                NavigationLink {
+                    TypeSelectView()
+                } label: {
+                    Text("Click")
+                }
+                
+                
+            }
+        }
     }
 }
 
--- Decaffeine/View(Interface)/TypeSelectView.swift
+++ Decaffeine/View(Interface)/TypeSelectView.swift
@@ -9,9 +9,46 @@ import SwiftUI
 
 struct TypeSelectView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 24) {
+            
+            VStack(spacing: 0){
+                Image("HotCoffee")
+                    .resizable()
+                    .scaledToFit()
+     
+                Text("Coffee")
+                    .padding(.bottom, 20)
+            }
+            .overlay(RoundedRectangle(cornerRadius: 10)
+                .stroke(Color.black, lineWidth: 4))
+            
+            VStack(spacing: 0){
+                Image("HotTea")
+                    .resizable()
+                    .scaledToFit()
+     
+                Text("Tea")
+                    .padding(.bottom, 20)
+            }
+            .overlay(RoundedRectangle(cornerRadius: 10)
+                .stroke(Color.black, lineWidth: 4))
+            
+            VStack(spacing: 0){
+                Image("Drink")
+                    .resizable()
+                    .scaledToFit()
+     
+                Text("Drink")
+                    .padding(.bottom, 20)
+            }
+            .overlay(RoundedRectangle(cornerRadius: 10)
+                .stroke(Color.black, lineWidth: 4))
+            
+        }
+        .navigationBarBackButtonHidden(true)
     }
 }
+
 
 struct TypeSelectView_Previews: PreviewProvider {
     static var previews: some View {
