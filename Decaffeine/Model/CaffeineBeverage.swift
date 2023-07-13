f7bb397e03e174913ca111ae8710bab0332c227f
Jun <wnsgur4092@gmail.com>
2023-07-13 16:52:56 +1000

    update. CaffeineBeverage Model

--- Decaffeine/Model/CaffeineBeverage.swift
+++ Decaffeine/Model/CaffeineBeverage.swift
@@ -7,33 +7,42 @@
 
 import Foundation
 
-struct CaffeineBeverage {
-    let coffee: Coffee
-    let tea: Tea
-    let drink: Drink
+// Enum for Beverage Types
+enum BeverageType {
+    case coffee(CoffeeType)
+    case tea(TeaType)
+    case drink(Drink)
 }
 
-struct Coffee {
-    let hotCoffee: CoffeeType
-    let coldCoffee: CoffeeType
+// Struct for Coffee Type
+enum CoffeeType {
+    case cold(coffeeName: String, coffeeImage: String)
+    case hot(coffeeName: String, coffeeImage: String)
 }
 
-struct CoffeeType {
-    let coffeeName: String
-    let coffeeImage: String 
+// Struct for Tea Type
+enum TeaType {
+    case cold(teaName: String, teaImage: String)
+    case hot(teaName: String, teaImage: String)
 }
 
-struct Tea {
-    let hotTea: TeaType
-    let coldTea: TeaType
-}
-
-struct TeaType {
-    let teaName: String
-    let teaImage: String 
-}
-
+// Struct for Drink
 struct Drink {
     let drinkName: String
-    let drinkImage : String
+    let drinkImage: String
+}
+
+// Struct for Caffeinated Beverage
+struct CaffeinatedBeverage {
+    let beverageType: BeverageType
+}
+
+extension CaffeinatedBeverage {
+    static let caffeinatedBeverage : [CaffeinatedBeverage] = [
+        CaffeinatedBeverage(beverageType: .coffee(.cold(coffeeName: "Iced Long Black", coffeeImage: "IceCoffee"))),
+        CaffeinatedBeverage(beverageType: .coffee(.cold(coffeeName: "Iced Latte", coffeeImage: "IceCoffee"))),
+        CaffeinatedBeverage(beverageType: .coffee(.hot(coffeeName: "Cappuccino", coffeeImage: "HotCoffee"))),
+        CaffeinatedBeverage(beverageType: .coffee(.hot(coffeeName: "Latte", coffeeImage: "HotCoffee"))),
+        CaffeinatedBeverage(beverageType: .drink(Drink(drinkName: "Redbull", drinkImage: "Drink")))
+    ]
 }
