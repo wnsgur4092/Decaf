8a803724c6f023f981a0f6b8fcf4dce910211f8a
Jun <wnsgur4092@gmail.com>
2023-07-15 00:14:07 +1000

    refactoring. TypeSelectView

--- Decaffeine/View(Interface)/TypeSelectView.swift
+++ Decaffeine/View(Interface)/TypeSelectView.swift
@@ -11,38 +11,11 @@ struct TypeSelectView: View {
     var body: some View {
         VStack(spacing: 24) {
             
-            VStack(spacing: -10){ // decrease this value
-                Image("HotCoffee")
-                    .resizable()
-                    .scaledToFit()
-     
-                Text("Coffee")
-                    .padding(.bottom, 20)
-            }
-            .overlay(RoundedRectangle(cornerRadius: 12)
-                .stroke(Color.black, lineWidth: 4))
+            TypeSelectCard(typeImageName: "HotCoffee", typeName: "Coffee")
             
-            VStack(spacing: -10){ // decrease this value
-                Image("HotTea")
-                    .resizable()
-                    .scaledToFit()
-     
-                Text("Tea")
-                    .padding(.bottom, 20)
-            }
-            .overlay(RoundedRectangle(cornerRadius: 12)
-                .stroke(Color.black, lineWidth: 4))
+            TypeSelectCard(typeImageName: "HotTea", typeName: "Tea")
             
-            VStack(spacing: -10){ // decrease this value
-                Image("Drink")
-                    .resizable()
-                    .scaledToFit()
-     
-                Text("Drink")
-                    .padding(.bottom, 20)
-            }
-            .overlay(RoundedRectangle(cornerRadius: 12)
-                .stroke(Color.black, lineWidth: 4))
+            TypeSelectCard(typeImageName: "Drink", typeName: "Drink")
             
         }
         .padding(.vertical, 50)
@@ -50,7 +23,28 @@ struct TypeSelectView: View {
     }
 }
 
+//MARK: - COMPONENTS
+struct TypeSelectCard : View {
+    var typeImageName : String
+    var typeName : String
+    
+    var body : some View {
+        VStack(spacing: -10){ // decrease this value
+            Image(typeImageName)
+                .resizable()
+                .scaledToFit()
+            
+            Text(typeName)
+                .padding(.bottom, 20)
+        }
+        .overlay(RoundedRectangle(cornerRadius: 12)
+            .stroke(Color.black, lineWidth: 4))
+        
+    }
+}
 
+
+//MARK: - PREVIEW
 struct TypeSelectView_Previews: PreviewProvider {
     static var previews: some View {
         TypeSelectView()
