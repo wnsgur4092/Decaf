98d3bdade0459c5dd6c53ee6277c4399fe24f71f
Jun <wnsgur4092@gmail.com>
2023-07-19 17:29:32 +1000

    refactoring. CoffeeAddView

--- Decaffeine/View(Interface)/NewItemPage/CoffeeAddView.swift
+++ Decaffeine/View(Interface)/NewItemPage/CoffeeAddView.swift
@@ -8,24 +8,19 @@
 import SwiftUI
 
 struct CoffeeAddView: View {
-    //MARK: - PROPERTEIS
-    let sizes : [String] = ["S", "M", "L"]
-    @State var selectedSize = "Small"
-    @State private var selectedSizeIndex = 0
-    
+    //MARK: - PROPERTIES
     @State var coffeeName : String
+    @State var selectedSize : String = "S"
+    @State var isHotSelected : Bool = true
+    @State var numberOfShots : Double = 1.0
     
-    //MARK: - BODY
     var body: some View {
         VStack{
             ScrollView{
                 VStack{
-                    Image("HotCoffee")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 150)
+                    ImageView(image: isHotSelected ? "HotCoffee" :"IceCoffee")
                     
-                    HotAndIcedSeclection()
+                    HotAndIcedSection(isHotSelecting: $isHotSelected)
                     
                     TextField("Coffee Name", text: $coffeeName)
                         .padding()
@@ -39,159 +34,35 @@ struct CoffeeAddView: View {
                         )
                     
                     
-                    sizeSelection
+                    SizeSelection(selectedSize: $selectedSize)
                     
-                    
-                    VStack(alignment: .leading, spacing: 20) {
-                        HStack{
-                            Text("SHOTS")
-                            
-                            Spacer()
-                            
-                            Button {
-                                
-                            } label: {
-                                Text("MORE..")
-                            }
-                            
-                        }
-                        
-                        HStack {
-                            Image("coffeeBean")
-                            Image("coffeeBean")
-                            Image("coffeeBean")
-                            Image("coffeeBean")
-                            
-                            Spacer()
-                            
-                            HStack {
-                                Button(action: {
-                                    // decrease action
-                                }) {
-                                    Image(systemName: "minus.square")
-                                }
-                                
-                                Text("1")
-                                    .padding(.horizontal)
-                                
-                                Button(action: {
-                                    // increase action
-                                }) {
-                                    Image(systemName: "plus.square")
-                                }
-                            }
-                        }
-                    }
-                    .frame(maxWidth: .infinity)
-                    .padding() // Add some padding around the VStack
-                    .background(
-                        RoundedRectangle(cornerRadius: 10) // The rounded rectangle
-                            .fill(Color.white) // Make it white
-                    )
-                    
-                    
-                    
-                    
-                    
-                    
-                    
+                    ContentSection(contentImage: "coffeebean", contentName: "Shots", numberOfContent: $numberOfShots)
                     
                 }
             }
-            
-            HStack{
-                Image("HotCoffee")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width:44, height: 44)
-                
-        
-                VStack{
-                    Text(coffeeName)
-                    
-                    HStack{
-                        Text("S")
-                        
-                        Text("156mg")
-                    }
-                }
-                
-                Spacer()
-                
-                Text("Save")
-                     .padding() // Add some padding around the Text
-                     .background(
-                         RoundedRectangle(cornerRadius: 10) // The rounded rectangle
-                             .fill(Color.black) // Make it black
-                     )
-                     .foregroundColor(.white) // Make the text white
-            }
+            FooterView(beverageImageName: isHotSelected ? "HotCoffee" :"IceCoffee", beverageName: $coffeeName)
         }
-        .background(Color("BackgroundColor").edgesIgnoringSafeArea(.all))
+        .onChange(of: selectedSize, perform: { value in
+            adjustShotsBasedOnSize()
+        })
+        //        .background(Color("BackgroundColor").edgesIgnoringSafeArea(.all))
     }
     
-    
-    //MARK: - COMPONENT
-    
-    fileprivate var sizeSelection: some View {
-        VStack(alignment: .leading){
-            Text("SIZE")
-            
-            HStack(spacing: 40) {
-                ForEach(Array(sizes.indices), id: \.self) { index in
-                    
-                    Button {
-                        selectedSizeIndex = index
-                    } label: {
-                        Text(sizes[index])
-                            .foregroundColor(.gray)
-                            .padding(.vertical, 20)
-                            .padding(.horizontal, 40)
-                            .font(.system(size: 14).bold())
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 2)
-                            )
-                    }
-                }
-            }
-            .frame(maxWidth:.infinity)
+    // whenever the selectedSize changes, adjust numberOfShots accordingly
+    private func adjustShotsBasedOnSize() {
+        switch selectedSize {
+        case "S":
+            numberOfShots = 1.0
+        case "M":
+            numberOfShots = 2.0
+        case "L":
+            numberOfShots = 3.0
+        default:
+            break
         }
-        .frame(maxWidth:.infinity)
-        .padding() // Add some padding around the VStack
-        .background(
-            RoundedRectangle(cornerRadius: 10) // The rounded rectangle
-                .fill(Color.white) // Make it white
-        )
     }
 }
 
-struct HotAndIcedSeclection : View {
-    var body : some View {
-        HStack{
-            Text("HOT")
-                .frame(maxWidth: .infinity)
-                .foregroundColor(.gray)
-                .padding(.vertical, 20)
-                .padding(.horizontal, 40)
-                .font(.system(size: 14).bold())
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 2)
-                )
-            
-            Spacer()
-            
-            Text("ICED")
-                .frame(maxWidth: .infinity)
-                .foregroundColor(.gray)
-                .padding(.vertical, 20)
-                .padding(.horizontal, 40)
-                .font(.system(size: 14).bold())
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 2)
-                )
-        }
-    }
-}
 
 
 //MARK: - PROPERTIES
